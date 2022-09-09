8619dffd62f10856e52550cd2cfb268baf6aac6b
FehmiCitiloglu <fehmicitiloglu@gmail.com>
2022-09-09 21:40:32 +0300

    first commit

---
+++ App.js
@@ -0,0 +1,20 @@
+import { Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native'
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import WelcomeScreen from './screens/WelcomeScreen';
+import UserScreen from './screens/UserScreen';
+
+
+const Drawer = createDrawerNavigator()
+
+export default function App() {
+
+  return (
+    <NavigationContainer>
+      <Drawer.Navigator >
+        <Drawer.Screen name={"Welcome"} component={WelcomeScreen} />
+        <Drawer.Screen name={"User"} component={UserScreen} />
+      </Drawer.Navigator>
+    </NavigationContainer>
+  )
+}
